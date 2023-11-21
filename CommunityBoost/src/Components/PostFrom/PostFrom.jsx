010a0e68886cb1bb385969c3b9b821a7117b52d5
plaze <banikakash131@gamil.com>
2023-11-21 17:18:56 +0530

    feat: upload data to database added

--- CommunityBoost/src/Components/PostFrom/PostFrom.jsx
+++ CommunityBoost/src/Components/PostFrom/PostFrom.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
+
+import axios from "axios";
+
 import "./style.css";
 const PostFrom = ({ setClicked }) => {
+  const [description, setDescripton] = useState("");
+  const [image, setImage] = useState(null);
+
+  const fileChangedhandler = (e) => {
+    const file = e.target.files[0];
+    setImage(file);
+  };
   const handleClick = (e) => {
     e.preventDefault();
+    const data = {
+      id: 4,
+      user: "Jhon Doe",
+      profilePic:
+        "https://images.unsplash.com/photo-1700344207586-7374ef84638a?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      description: description,
+      profile: "profile",
+      contentImage: image,
+    };
+    axios.post("http://localhost:3500/posts", data);
     setClicked(false);
   };
 
@@ -15,9 +36,21 @@ const PostFrom = ({ setClicked }) => {
             id="postFrom__description"
             className="postFrom__description"
             placeholder="Describe your project"
+            value={description}
+            onChange={(e) => {
+              setDescripton(e.target.value);
+            }}
           />
           <div className="postFrom__button">
-            <button>Upload photo</button>
+            <input
+              type="file"
+              name="image"
+              id="postFrom__image"
+              className="postFrom__image"
+              onChange={(e) => {
+                fileChangedhandler(e);
+              }}
+            />
             <button onClick={(e) => handleClick(e)}>Post</button>
           </div>
         </form>
