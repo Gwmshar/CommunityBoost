c843c01afa1f640067b8124f519e9f8edbfac102
plaze <banikakash131@gamil.com>
2023-11-21 12:10:21 +0530

    feat: add feedCard

---
+++ CommunityBoost/src/Components/FeedCard/FeedCard.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Card from "../../Card/Card";
+
+const FeedCard = () => {
+  const [posts, setPosts] = useState([]); // Renamed 'post' to 'posts' for clarity
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await axios.get("http://localhost:3500/posts"); // Added 'http://' to the URL
+        setPosts(res.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+    fetchData();
+  }, []); // Empty dependency array to fetch data only on initial component mount
+
+  return (
+    <div>
+      {posts.map((data) => (
+        <Card key={data.id} data={data} />
+      ))}
+    </div>
+  );
+};
+
+export default FeedCard;
